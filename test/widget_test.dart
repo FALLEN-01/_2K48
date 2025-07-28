ba403b2c41fc2d57b63b517489370a713c28567c
Jeffin Basil <jeffinbasil484@gmail.com>
2025-07-28 21:51:49 +0530

    made branch android focused

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:_2k48/main.dart';
+import 'package:game_2k48/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
