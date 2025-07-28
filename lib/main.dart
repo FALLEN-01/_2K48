2b0d7571bd7eaccc7f97077d12b8da877000cbd2
Jeffin Basil <jeffinbasil484@gmail.com>
2025-07-28 22:11:44 +0530

    made it ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,470 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '2048 Game',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Game2048(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Game2048 extends StatefulWidget {
+  const Game2048({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Game2048> createState() => _Game2048State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _Game2048State extends State<Game2048> {
+  int gridSize = 4; // Default to 4x4
+  List<List<int>> grid = List.generate(4, (_) => List.filled(4, 0));
+  int score = 0;
+  bool gameOver = false;
+  bool won = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _initializeGame();
+  }
+
+  void _initializeGame() {
+    grid = List.generate(gridSize, (_) => List.filled(gridSize, 0));
+    score = 0;
+    gameOver = false;
+    won = false;
+    _addRandomTile();
+    _addRandomTile();
+    setState(() {});
+  }
+
+  void _changeGridSize(int newSize) {
+    gridSize = newSize;
+    _initializeGame();
+  }
+
+  void _addRandomTile() {
+    List<Point<int>> emptyCells = [];
+    for (int i = 0; i < gridSize; i++) {
+      for (int j = 0; j < gridSize; j++) {
+        if (grid[i][j] == 0) {
+          emptyCells.add(Point(i, j));
+        }
+      }
+    }
+
+    if (emptyCells.isNotEmpty) {
+      final random = Random();
+      final cell = emptyCells[random.nextInt(emptyCells.length)];
+      grid[cell.x][cell.y] = random.nextDouble() < 0.9 ? 2 : 4;
+    }
+  }
+
+  bool _canMove() {
+    // Check for empty cells
+    for (int i = 0; i < gridSize; i++) {
+      for (int j = 0; j < gridSize; j++) {
+        if (grid[i][j] == 0) return true;
+      }
+    }
+
+    // Check for possible merges
+    for (int i = 0; i < gridSize; i++) {
+      for (int j = 0; j < gridSize; j++) {
+        if (j < gridSize - 1 && grid[i][j] == grid[i][j + 1]) return true;
+        if (i < gridSize - 1 && grid[i][j] == grid[i + 1][j]) return true;
+      }
+    }
+
+    return false;
+  }
+
+  void _moveLeft() {
+    bool moved = false;
+    for (int i = 0; i < gridSize; i++) {
+      List<int> row = grid[i].where((cell) => cell != 0).toList();
+      for (int j = 0; j < row.length - 1; j++) {
+        if (row[j] == row[j + 1]) {
+          row[j] *= 2;
+          score += row[j];
+          if (row[j] == 2048 && !won) {
+            won = true;
+            _showWinDialog();
+          }
+          row.removeAt(j + 1);
+        }
+      }
+      while (row.length < gridSize) {
+        row.add(0);
+      }
+
+      for (int j = 0; j < gridSize; j++) {
+        if (grid[i][j] != row[j]) moved = true;
+        grid[i][j] = row[j];
+      }
+    }
+
+    if (moved) {
+      _addRandomTile();
+      setState(() {});
+      if (!_canMove()) {
+        gameOver = true;
+        _showGameOverDialog();
+      }
+    }
+  }
+
+  void _moveRight() {
+    bool moved = false;
+    for (int i = 0; i < 4; i++) {
+      List<int> row = grid[i].where((cell) => cell != 0).toList();
+      for (int j = row.length - 1; j > 0; j--) {
+        if (row[j] == row[j - 1]) {
+          row[j] *= 2;
+          score += row[j];
+          if (row[j] == 2048 && !won) {
+            won = true;
+            _showWinDialog();
+          }
+          row.removeAt(j - 1);
+          j--;
+        }
+      }
+      while (row.length < 4) {
+        row.insert(0, 0);
+      }
+
+      for (int j = 0; j < 4; j++) {
+        if (grid[i][j] != row[j]) moved = true;
+        grid[i][j] = row[j];
+      }
+    }
+
+    if (moved) {
+      _addRandomTile();
+      setState(() {});
+      if (!_canMove()) {
+        gameOver = true;
+        _showGameOverDialog();
+      }
+    }
+  }
+
+  void _moveUp() {
+    bool moved = false;
+    for (int j = 0; j < 4; j++) {
+      List<int> column = [];
+      for (int i = 0; i < 4; i++) {
+        if (grid[i][j] != 0) column.add(grid[i][j]);
+      }
+
+      for (int i = 0; i < column.length - 1; i++) {
+        if (column[i] == column[i + 1]) {
+          column[i] *= 2;
+          score += column[i];
+          if (column[i] == 2048 && !won) {
+            won = true;
+            _showWinDialog();
+          }
+          column.removeAt(i + 1);
+        }
+      }
+      while (column.length < 4) {
+        column.add(0);
+      }
+
+      for (int i = 0; i < 4; i++) {
+        if (grid[i][j] != column[i]) moved = true;
+        grid[i][j] = column[i];
+      }
+    }
+
+    if (moved) {
+      _addRandomTile();
+      setState(() {});
+      if (!_canMove()) {
+        gameOver = true;
+        _showGameOverDialog();
+      }
+    }
+  }
+
+  void _moveDown() {
+    bool moved = false;
+    for (int j = 0; j < 4; j++) {
+      List<int> column = [];
+      for (int i = 0; i < 4; i++) {
+        if (grid[i][j] != 0) column.add(grid[i][j]);
+      }
+
+      for (int i = column.length - 1; i > 0; i--) {
+        if (column[i] == column[i - 1]) {
+          column[i] *= 2;
+          score += column[i];
+          if (column[i] == 2048 && !won) {
+            won = true;
+            _showWinDialog();
+          }
+          column.removeAt(i - 1);
+          i--;
+        }
+      }
+      while (column.length < 4) {
+        column.insert(0, 0);
+      }
+
+      for (int i = 0; i < 4; i++) {
+        if (grid[i][j] != column[i]) moved = true;
+        grid[i][j] = column[i];
+      }
+    }
+
+    if (moved) {
+      _addRandomTile();
+      setState(() {});
+      if (!_canMove()) {
+        gameOver = true;
+        _showGameOverDialog();
+      }
+    }
+  }
+
+  void _showWinDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('🎉 You Win!'),
+          content: const Text(
+            'You reached 2048! Continue playing or start a new game.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Continue'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _initializeGame();
+              },
+              child: const Text('New Game'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showGameOverDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('💀 Game Over'),
+          content: Text('Final Score: $score\nTry again?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _initializeGame();
+              },
+              child: const Text('New Game'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Color _getTileColor(int value) {
+    switch (value) {
+      case 0:
+        return Colors.grey[300]!;
+      case 2:
+        return Colors.grey[100]!;
+      case 4:
+        return Colors.grey[200]!;
+      case 8:
+        return Colors.orange[200]!;
+      case 16:
+        return Colors.orange[300]!;
+      case 32:
+        return Colors.orange[400]!;
+      case 64:
+        return Colors.orange[500]!;
+      case 128:
+        return Colors.orange[600]!;
+      case 256:
+        return Colors.orange[700]!;
+      case 512:
+        return Colors.orange[800]!;
+      case 1024:
+        return Colors.orange[900]!;
+      case 2048:
+        return Colors.red[600]!;
+      default:
+        return Colors.red[800]!;
+    }
+  }
+
+  Color _getTextColor(int value) {
+    return value <= 4 ? Colors.grey[700]! : Colors.white;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('2048'),
+        backgroundColor: Colors.orange[600],
+        foregroundColor: Colors.white,
+        elevation: 0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          // Score and New Game button
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.orange[600],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    'Score: $score',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _initializeGame,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange[600],
+                    foregroundColor: Colors.white,
+                  ),
+                  child: const Text('New Game'),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+
+          // Game instructions
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'Swipe to move tiles. When two tiles with the same number touch, they merge into one!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          // Game Grid
+          Expanded(
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: GestureDetector(
+                  onPanEnd: (details) {
+                    if (gameOver) return;
+
+                    final velocity = details.velocity.pixelsPerSecond;
+                    final dx = velocity.dx;
+                    final dy = velocity.dy;
+
+                    if (dx.abs() > dy.abs()) {
+                      if (dx > 0) {
+                        _moveRight();
+                      } else {
+                        _moveLeft();
+                      }
+                    } else {
+                      if (dy > 0) {
+                        _moveDown();
+                      } else {
+                        _moveUp();
+                      }
+                    }
+                  },
+                  child: SizedBox(
+                    width: 320,
+                    height: 320,
+                    child: GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            crossAxisSpacing: 8,
+                            mainAxisSpacing: 8,
+                          ),
+                      itemCount: 16,
+                      itemBuilder: (context, index) {
+                        final i = index ~/ 4;
+                        final j = index % 4;
+                        final value = grid[i][j];
+
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: _getTileColor(value),
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: Center(
+                            child: value == 0
+                                ? null
+                                : Text(
+                                    '$value',
+                                    style: TextStyle(
+                                      fontSize: value >= 1000 ? 24 : 32,
+                                      fontWeight: FontWeight.bold,
+                                      color: _getTextColor(value),
+                                    ),
+                                  ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // Controls hint
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Swipe in any direction to move tiles',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
